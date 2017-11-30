541b0ee74eff744de1e354c7d413fbd855b9d479
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-30 03:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var newID = (1000000000 + oldID)
 
 var ageIsValid = Number.isInteger (ageIsValid)
 
-var randomNumber = 20 * Math.random();
+var randomNumber = Math.floor(20 * Math.random());
 
 var randomInteger = Math.floor(Math.random() * 20);
 
